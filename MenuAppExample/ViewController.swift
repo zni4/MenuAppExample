82ae69678b8b230886c47a54fec6377a0fa27305
zni4 <zni4@hotmail.com>
2019-04-25 13:17:53 +0200

    Initial Commit

---
+++ MenuAppExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuAppExample
+//
+//  Created by Ignacio on 25/04/2019.
+//  Copyright © 2019 Ignacio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
