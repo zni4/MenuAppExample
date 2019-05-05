a5fb46c9b1027458e560fbaa61dbabb83e7c98d2
zni4 <zni4@hotmail.com>
2019-05-05 17:33:40 +0200

    Controlamos si estamos en modo claro u oscuro

--- MenuAppExample/TestViewController.swift
+++ MenuAppExample/TestViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 class TestViewController: NSViewController {
-
+    
     @IBOutlet weak var testLabel: NSTextField! //Asociamos la etiqueta a su nombre
     
     override func viewDidLoad() {
@@ -27,14 +27,21 @@ extension TestViewController {
     
     @IBAction func changeLabel(_ sender: NSButton) {
         
+        let osxMode = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") ?? "Light"
+        
         if testLabel.stringValue == "Cambio" {
             testLabel.stringValue = "TEST POPOVER"
             testLabel.textColor = NSColor(deviceRed: 0, green: 0, blue: 0, alpha: 100)
             testLabel.font = NSFont(name: testLabel.font!.fontName, size: 12)
         } else {
             testLabel.stringValue = "Cambio"
-            testLabel.textColor = NSColor(deviceRed: 255, green: 255, blue: 0, alpha: 100)
-            testLabel.font = NSFont(name: testLabel.font!.fontName, size: 8)
+            if osxMode == "Light" {
+                testLabel.textColor = NSColor(deviceRed: 255, green: 255, blue: 0, alpha: 100)
+                testLabel.font = NSFont(name: testLabel.font!.fontName, size: 8)
+            } else {
+                testLabel.textColor = NSColor(deviceRed: 0, green: 0, blue: 255, alpha: 100)
+                testLabel.font = NSFont(name: testLabel.font!.fontName, size: 18)
+            }
         }
         
     }
