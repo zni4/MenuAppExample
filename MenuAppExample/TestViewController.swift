47ce5354dff0bbe060e9c341da4c565d8a3d3eb4
zni4 <zni4@hotmail.com>
2019-05-05 17:39:29 +0200

    Comprobamos si el sistema está en modo claro u oscuro

--- MenuAppExample/TestViewController.swift
+++ MenuAppExample/TestViewController.swift
@@ -34,11 +34,12 @@ extension TestViewController {
             testLabel.textColor = NSColor(deviceRed: 0, green: 0, blue: 0, alpha: 100)
             testLabel.font = NSFont(name: testLabel.font!.fontName, size: 12)
         } else {
-            testLabel.stringValue = "Cambio"
             if osxMode == "Light" {
-                testLabel.textColor = NSColor(deviceRed: 255, green: 255, blue: 0, alpha: 100)
-                testLabel.font = NSFont(name: testLabel.font!.fontName, size: 8)
+                testLabel.stringValue = "Claro"
+                testLabel.textColor = NSColor(deviceRed: 255, green: 0, blue: 0, alpha: 100)
+                testLabel.font = NSFont(name: testLabel.font!.fontName, size: 18)
             } else {
+                testLabel.stringValue = "Oscuro"
                 testLabel.textColor = NSColor(deviceRed: 0, green: 0, blue: 255, alpha: 100)
                 testLabel.font = NSFont(name: testLabel.font!.fontName, size: 18)
             }
